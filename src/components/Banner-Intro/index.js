9c6b4a3dec49a00a32bc829ac491cf3306e4907f
= <=>
2023-06-01 20:39:26 +0700

    123

--- src/components/Banner-Intro/index.js
+++ src/components/Banner-Intro/index.js
@@ -1,23 +1,40 @@
 import styles from './banner-intro.module.scss'
 import classNames from 'classnames/bind'
+import { GrLinkNext } from "react-icons/gr";
+import { Link } from 'react-router-dom'
+import BannerSource from '../Banner-source'
+
 
 const cx = classNames.bind(styles);
 
 const BannerIntro = () => {
+
     return (
-        <section className={cx("home")}>
+        <div className={cx('wrapper')}>
+            <div className={cx('all-function')}>
 
-            <div className={cx("image-banner")}>
-                <img src={require('./images/about-img.png')} alt="" />
+                <BannerSource
+                    linearColor={'linear-gradient(to right, rgb(122, 115, 19), rgb(255, 186, 164))'}
+                    title={'Cấu trúc code PHP cơ bản'}
+                    content={'Bài viết này nhằm tổng hợp lại các dự án mà học viên F8 đã hoàn thành và chia sẻ trên nhóm Học lập trình web F8. Các dự án dưới đây được mình ngẫu nhiên lựa chọn để đăng chứ không mang tính xếp hạng các bạn nhé.'}
+                    urlImage={'https://files.fullstack.edu.vn/f8-prod/banners/Banner_web_ReactJS.png'} />
             </div>
 
-            <div className={cx("content")}>
-                <h3>your course to success</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa cumque neque quam amet perferendis sed rem ut tenetur porro praesentium.</p>
-                <a href="#" className={cx("btn")}>get started</a>
-            </div>
+            <div className={cx('inner')}>
+                <img src={require('./images/banner.png')} />
+                <div className={cx('spotilne-linked')}>
+                    <p>Are you want to build this source in 1 min ?</p>
+                    <GrLinkNext className={cx('icon-linked')} />
+                </div>
+                <div className={cx('sologan')}>
+                    <h1>Get my source to improve your talent</h1>
+                    <p>What is my source?</p>
+                </div>
+                <span className={cx('descript')} >Bắt ngay cơ hội này để không bỏ lỡ mức lương lên đến 14 triệu, các khoản trợ cấp, thưởng hấp dẫn, các chuyến du lịch, nghỉ mát, team building, loạt phúc lợi đỉnh,...</span>
+                <Link to='/all-sources' className={cx('btn-start')}>Xem ngay</Link>
 
-        </section>
+            </div>
+        </div>
     )
 }
 export default BannerIntro
--- src/pages/intro/index.js
+++ src/pages/intro/index.js
@@ -1,5 +1,4 @@
 import BannerIntro from '../../components/Banner-Intro';
-import CartContext from '../../components/CardContext';
 import styles from './intro.module.scss'
 import classNames from 'classnames/bind'
 const cx = classNames.bind(styles);
@@ -7,12 +6,7 @@ const cx = classNames.bind(styles);
 const Intro = () => {
     return (<div className={cx('wrapper')} >
         <BannerIntro />
-        <div className={cx('contain-card-intro')}>
-            <CartContext />
-            <CartContext />
-            <CartContext />
-            <CartContext />
-        </div>
+       
     </div>)
 }
 export default Intro;
